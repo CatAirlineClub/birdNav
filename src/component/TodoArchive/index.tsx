72899e0e323237be0d4d04e85cc2be76428bf645
York Xiang <bombless@126.com>
2022-01-29 15:03:16 +0800

    进行中：重构待办归档

--- src/component/TodoArchive/index.tsx
+++ src/component/TodoArchive/index.tsx
@@ -9,13 +9,13 @@ import {
 import { MouseEventHandler, useState } from "react";
 import {
   channelArchive as todoChannelArchive,
-  channelLeftView as todoChannelView,
+  channelLeftView as todoLeftView,
   TodoListNode,
   TodoListTree,
 } from "../../logic/todo";
 
 const { receiver: todoArchiveReceiver } = todoChannelArchive;
-const { sender: todoViewSender } = todoChannelView;
+const { sender: todoLeftViewSender } = todoLeftView;
 
 interface Props {
   onMouseEnter: MouseEventHandler<HTMLDivElement>;
@@ -63,8 +63,8 @@ const TodoArchive = (props: Props) => {
     setDepth(depth - 1);
   }
 
-  function view() {
-    todoViewSender.ping().then((cb) => {
+  function toLeftView() {
+    todoLeftViewSender.ping().then((cb) => {
       cb(openingTodoList!);
       setOpeningMode(false);
     });
@@ -155,7 +155,7 @@ const TodoArchive = (props: Props) => {
       })}
       <ArrowLeftUp
         className={resolveClasses(openingMode ? "" : "remove")}
-        onClick={view}
+        onClick={toLeftView}
         theme="outline"
         size="30"
         fill="slateblue"
