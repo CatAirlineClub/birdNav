1c8fa566e6916764bb70c3be467e79de09916f6e
York Xiang <bombless@126.com>
2022-01-29 13:50:26 +0800

    重写月度计划

---
+++ src/component/TodoList/TodoView.tsx
@@ -0,0 +1,287 @@
+import { useLocalStorageState } from "@/hooks/useLocalStorageState";
+import { UserAppList } from "@/store/app";
+import { defaultLongTodoList } from "@/store/todo";
+import resolveClasses from "@/utils/resolveClasses";
+import { CheckOne, CloseOne, Plus, ReduceOne } from "@icon-park/react";
+import { nanoid } from "nanoid";
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import { channelArchive as todoChannelArchive } from "../../logic/todo";
+import { channelView as todoChannelView } from "../../logic/todo";
+import { TodoItem, DoneItem, TodoListNode } from "../../logic/todo";
+
+const { sender: todoArchiveSender } = todoChannelArchive;
+const { receiver: todoViewReceiver } = todoChannelView;
+
+const todoClone: TodoListNode = {
+  title: "月度计划",
+  todoList: defaultLongTodoList,
+  doneList: [],
+};
+
+interface TodoProps {
+  data: TodoListNode;
+  setData: (value: TodoListNode) => void;
+}
+
+const LongTimeTodo = ({ data: todoData, setData: setTodoData }: TodoProps) => {
+  const [visible, setVisibility] = useState(true);
+  const [type, setType] = useState("todo");
+  const [title, setTitle] = useState("月度计划");
+
+  todoViewReceiver.pong((item) => {
+    setTodoData(item);
+    setVisibility(true);
+  });
+
+  const addTodoItem = (content: { title: string; createTime: number }) => {
+    if (!content.title) return;
+    const newTodoItem = { id: nanoid(), ...content };
+    const newTodoList = [newTodoItem, ...todoData.todoList];
+    const newTodoData = Object.assign(
+      {},
+      { title, todoList: newTodoList, doneList: todoData.doneList }
+    );
+    setTodoData(newTodoData);
+    todoClone.todoList = newTodoList.filter((_) => true);
+  };
+
+  const deleteTodoItem = (item: TodoItem) => {
+    const newTodoList = todoData.todoList.filter(
+      (todo: TodoItem) => todo.id !== item.id
+    );
+    const newTodoData = Object.assign(
+      {},
+      { title, todoList: newTodoList, doneList: todoData.doneList }
+    );
+    setTodoData(newTodoData);
+  };
+
+  const competeTodoItem = (item: TodoItem) => {
+    const newTodoList = todoData.todoList.filter(
+      (todo: TodoItem) => todo.id !== item.id
+    );
+    const newDoneList = [
+      {
+        ...item,
+        doneTime: new Date().getTime(),
+      },
+      ...(todoData.doneList as DoneItem[]),
+    ];
+    setTodoData({
+      title: todoData.title,
+      todoList: newTodoList,
+      doneList: newDoneList,
+    });
+  };
+
+  const deleteDoneItem = (item: TodoItem) => {
+    const newDoneList = todoData.doneList.filter((done) => done.id !== item.id);
+    setTodoData({
+      title: todoData.title,
+      todoList: todoData.todoList,
+      doneList: newDoneList,
+    });
+  };
+
+  const redoDoneItem = (item: TodoItem) => {
+    const newDoneList = todoData.doneList.filter(
+      (done: DoneItem) => done.id !== item.id
+    );
+    const newTodoList = [item, ...todoData.todoList];
+    setTodoData({
+      title: todoData.title,
+      todoList: newTodoList,
+      doneList: newDoneList,
+    });
+  };
+
+  return (
+    <section className={resolveClasses("todo", visible ? "" : "hide")}>
+      <TodoHeader
+        title={title}
+        type={type}
+        setType={setType}
+        setVisible={setVisibility}
+      />
+      {type === "todo" && (
+        <TodoContent
+          list={todoData.todoList}
+          onAdd={addTodoItem}
+          onDelete={deleteTodoItem}
+          onCompete={competeTodoItem}
+        />
+      )}
+      {type === "done" && (
+        <DoneContent
+          list={todoData.doneList}
+          onDelete={deleteDoneItem}
+          onRedo={redoDoneItem}
+        />
+      )}
+    </section>
+  );
+};
+
+interface TodoHeaderProps {
+  type: string;
+  title: string;
+  setType: (value: string) => void;
+  setVisible: (value: boolean) => void;
+}
+
+const TodoHeader: React.FC<TodoHeaderProps> = (props) => {
+  const { type, setType, setVisible, title } = props;
+
+  function save() {
+    todoArchiveSender.ping().then((callback) => {
+      callback(todoClone);
+    });
+  }
+
+  const date = new Date();
+
+  return (
+    <section className="todo-header">
+      <span className="todo-header-title">{title}</span>
+      <section className="todo-header-btns">
+        <section
+          className={resolveClasses("todo-header-btn center")}
+          onClick={() => {
+            save();
+            setVisible(false);
+          }}
+        >
+          Save
+        </section>
+        <section
+          className={resolveClasses(
+            "todo-header-btn center",
+            type === "todo" ? "active" : ""
+          )}
+          onClick={() => setType("todo")}
+        >
+          Todo
+        </section>
+        <section
+          className={resolveClasses(
+            "todo-header-btn center",
+            type === "todo" ? "" : "active"
+          )}
+          onClick={() => setType("done")}
+        >
+          Done
+        </section>
+      </section>
+    </section>
+  );
+};
+
+interface TodoContentProps {
+  list: Array<{
+    id: string;
+    title: string;
+    createTime: number;
+  }>;
+  onAdd: (content: { title: string; createTime: number }) => void;
+  onDelete: (item: TodoItem) => void;
+  onCompete: (item: TodoItem) => void;
+}
+
+const TodoContent: React.FC<TodoContentProps> = (props) => {
+  const { list, onAdd, onDelete, onCompete } = props;
+  const [text, setText] = useState("");
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    onAdd({
+      title: text,
+      createTime: new Date().getTime(),
+    });
+    setText("");
+  };
+
+  return (
+    <div className="TodoContent">
+      <ul className="TodoContent-list">
+        {list.map((item: TodoItem, index: number) => (
+          <li className="TodoContent-item" key={index}>
+            <section className="TodoContent-item-title">
+              {index + 1}、{item.title}
+            </section>
+            <section className="TodoContent-item-btns">
+              <div
+                className="TodoContent-item-btn"
+                onClick={() => onDelete(item)}
+              >
+                <CloseOne theme="outline" size="23" fill="rgb(196,196,196)" />
+              </div>
+              <div
+                className="TodoContent-item-btn"
+                onClick={() => onCompete(item)}
+              >
+                <CheckOne theme="outline" size="23" fill="rgb(196,196,196)" />
+              </div>
+            </section>
+          </li>
+        ))}
+      </ul>
+      <form className="TodoContent-add" onSubmit={onSubmit}>
+        <input
+          className={text.length > 0 ? "input-active" : "input-default"}
+          type="text"
+          placeholder="something"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="TodoContent-add-btn center" onClick={onSubmit}>
+          <Plus theme="outline" size="23" fill="rgb(196,196,196)" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+interface DoneContentProps {
+  list: Array<{
+    id: string;
+    title: string;
+    createTime: number;
+    doneTime: number;
+  }>;
+  onDelete: (item: TodoItem) => void;
+  onRedo: (item: TodoItem) => void;
+}
+
+const DoneContent: React.FC<DoneContentProps> = (props) => {
+  const { list, onDelete, onRedo } = props;
+
+  return (
+    <div className="DoneContent">
+      <ul className="DoneContent-list">
+        {list.map((item: DoneItem, index: number) => (
+          <li className="DoneContent-item" key={index}>
+            <section className="DoneContent-item-title">
+              {index + 1}、{item.title}
+            </section>
+            <section className="DoneContent-item-btns">
+              <div
+                className="DoneContent-item-btn"
+                onClick={() => onRedo(item)}
+              >
+                <ReduceOne theme="outline" size="23" fill="rgb(196,196,196)" />
+              </div>
+              <div
+                className="DoneContent-item-btn"
+                onClick={() => onDelete(item)}
+              >
+                <CloseOne theme="outline" size="23" fill="rgb(196,196,196)" />
+              </div>
+            </section>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default LongTimeTodo;
--- src/component/TodoList/index.tsx
+++ src/component/TodoList/index.tsx
@@ -1,11 +1,37 @@
 import LongTimeTodo from "./LongTimeTodo";
 import ShortTimeTodo from "./ShortTimeTodo";
+import View from "./TodoView";
 import "./index.css";
+import { useLocalStorageState } from "@/hooks/useLocalStorageState";
+import { defaultLongTodoList } from "@/store/todo";
+import { useState } from "react";
+import { DoneItem, TodoListNode } from "@/logic/todo";
 
 const TodoList = () => {
+  const [todoList, setTodoList] = useLocalStorageState(
+    "long-todolist",
+    defaultLongTodoList
+  );
+  const [doneList, setDoneList] = useLocalStorageState<DoneItem[]>(
+    "long-donelist",
+    []
+  );
+  const [title, setTitle] = useState("月度计划");
+  const [data, setData] = useState<TodoListNode>({
+    title: "月度计划",
+    todoList,
+    doneList,
+  });
+  function setDataWrap(data: TodoListNode) {
+    if (data.title == "月度计划") {
+      setTodoList(data.todoList);
+      setDoneList(data.doneList);
+    }
+    setData(data);
+  }
   return (
     <div className="todo-container rowcenter">
-      <LongTimeTodo></LongTimeTodo>
+      <View data={data} setData={setDataWrap}></View>
       <ShortTimeTodo></ShortTimeTodo>
     </div>
   );
